29a90fd5f7df387f24df179a8fc2de4f7884ca79
Thabiso <mtsharethabiso@gmail.com>
2020-05-08 03:23:25 +0200

    Edited environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBjEzux5DBjeHNGbTbcCNqJIdcb2N6XXEA",
+    authDomain: "zakoshop-ae30b.firebaseapp.com",
+    databaseURL: "https://zakoshop-ae30b.firebaseio.com",
+    projectId: "zakoshop-ae30b",
+    storageBucket: "zakoshop-ae30b.appspot.com",
+    messagingSenderId: "83220241482",
+    appId: "1:83220241482:web:b00d6df9aaa64459229ab3",
+    measurementId: "G-0F0MYTLW2P"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBjEzux5DBjeHNGbTbcCNqJIdcb2N6XXEA",
+    authDomain: "zakoshop-ae30b.firebaseapp.com",
+    databaseURL: "https://zakoshop-ae30b.firebaseio.com",
+    projectId: "zakoshop-ae30b",
+    storageBucket: "zakoshop-ae30b.appspot.com",
+    messagingSenderId: "83220241482",
+    appId: "1:83220241482:web:b00d6df9aaa64459229ab3",
+    measurementId: "G-0F0MYTLW2P"
+  }
 };
 
 /*
