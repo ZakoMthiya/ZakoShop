19d849515716dbe1725887674ba772ec3457876a
Thabiso <mtsharethabiso@gmail.com>
2020-05-12 06:55:25 +0200

    Added view product name and price in cart

--- src/app/shopping-cart/shopping-cart.component.ts
+++ src/app/shopping-cart/shopping-cart.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { ShoppingCartService } from '../shopping-cart.service';
 
 @Component({
   selector: 'app-shopping-cart',
@@ -7,9 +8,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingCartComponent implements OnInit {
 
-  constructor() { }
+  cart$;
+  idArray: string[];
+  itemsArray: any[];
+  oneItem;
+
+  constructor(private cartService: ShoppingCartService, private cdr: ChangeDetectorRef) { }
 
   ngOnInit() {
+    let cart$ = this.cartService.getCarti();
+    cart$.valueChanges().subscribe(cart => this.cart$ = cart);
+  }
+
+
+  getQuantity() {
+    let quantityToBeReturned = 0;
+    let iA = [];
+    let idA: string[] = [];
+
+    if(!this.cart$) {
+      console.log('No cart');
+      return quantityToBeReturned;
+    };
+
+    this.cart$.forEach(x => {
+
+      quantityToBeReturned += x.quantity;
+      iA.push(x.product);
+      idA.push(x.product.id);
+
+
+    });
+
+    // console.log(idA);
+    // console.log(iA);
+
+    // let tt;
+    // iA.forEach(a => {
+    //   if(a.id === idA[0]) tt = a
+    //   console.log(a.id === idA[0])
+    //   console.log(idA[0])
+    // })
+    // console.log('TT ' + tt.id);
+
+    this.idArray = idA;
+    this.itemsArray = iA;
+    // console.log(this.idArray);
+    // console.log(this.itemsArray)
+
+    return quantityToBeReturned;
   }
 
 }
